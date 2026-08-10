98b738f1a5e4d2c8837ccfd1c093d3e74c1ed7b3
NomanCyberSpace <nomanmuhammad35594@gmail.com>
2026-08-10 15:51:41 +0500

    Update repository link in github.js

--- commands/github.js
+++ commands/github.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 async function githubCommand(sock, chatId, message) {
   try {
-    const res = await fetch('https://api.github.com/repos/XHUNTERBOT/XHUNTERBOT-md');
+    const res = await fetch('https://github.com/NomanCyberSpace/XHunterBot');
     if (!res.ok) throw new Error('Error fetching repository data');
     const json = await res.json();
 
